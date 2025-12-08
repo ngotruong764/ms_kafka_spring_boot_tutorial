3858e25e5dec1a453f18e556b82e571d069dbde3
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2025-12-08 23:11:09 +0700

    consumer

---
+++ email_notification_ms/src/main/java/com/kafka_tutorial/email_notification_ms/EmailNotificationMsApplication.java
@@ -0,0 +1,13 @@
+package com.kafka_tutorial.email_notification_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotificationMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailNotificationMsApplication.class, args);
+    }
+
+}
