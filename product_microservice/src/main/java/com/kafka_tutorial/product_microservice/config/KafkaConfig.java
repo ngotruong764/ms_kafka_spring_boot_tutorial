c17adb739208c973cea8152e96b1ecb2eaa7cbad
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2025-12-08 08:21:20 +0700

    update kafka config java code

--- product_microservice/src/main/java/com/kafka_tutorial/product_microservice/config/KafkaConfig.java
+++ product_microservice/src/main/java/com/kafka_tutorial/product_microservice/config/KafkaConfig.java
@@ -1,14 +1,53 @@
 package com.kafka_tutorial.product_microservice.config;
 
+import com.kafka_tutorial.product_microservice.ProductCreatedEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
+
+    private Map<String, Object> producerConfig() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.ACKS_CONFIG, acks);
+        return config;
+    }
+
+    // Method that responsible for creating a factory object that use configuration properties
+    // to create Kafka Producer
+    @Bean
+    protected ProducerFactory<String, ProductCreatedEvent> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    protected KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+
     // Create method responsible for create a new topic
     @Bean
     public NewTopic newTopic() {
